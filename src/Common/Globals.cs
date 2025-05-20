21bb9c85d508242414e6a5495621271aa2bbf849
CauSeygn <smarasmr@gmail.com>
2025-05-20 20:35:13 +0200

    ported newloot1
    csharp shill

--- src/Common/Globals.cs
+++ src/Common/Globals.cs
@@ -15,3 +15,26 @@ public class Global
 
     public static UnderscoreGlobal _global = new();
 }
+
+public class Save
+{
+    public string questType;
+    public string questSubtype;
+    public double questCount;
+    public bool activityLoot;
+    public bool bouncyLoot;
+    public double arenaLoot;
+}
+
+public class House
+{
+    public Arena arena = new();
+}
+
+public class Arena
+{
+    public double showDamage(string index, double x, double y, double z)
+    {
+        return 0;
+    }
+}
